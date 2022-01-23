9e2699b4795e8d2a1c07d545cc71ac64fb22b106
David Durieux <david@durieux.family>
2022-01-23 16:46:37 +0100

    Continue tests of REST API

--- tests/RESTAPI/type/5_type-items-GET.js
+++ tests/RESTAPI/type/5_type-items-GET.js
@@ -27,14 +27,28 @@ describe('Endpoint /v1/cmdb/types/2/items (GET all)', function() {
       assert(is.string(firstElement.name));
       assert(is.string(firstElement.created_at));
       assert(validator.isISO8601(firstElement.created_at));
-      // TODO manage updated_at can be a date or null
+      okType = false;
+      if (is.null(firstElement.updated_at)) {
+        okType = true;
+      } else if (is.string(firstElement.updated_at)) {
+        okType = true;
+        assert(validator.isISO8601(firstElement.updated_at));
+      }
+      assert(okType, 'updated_at must be a string or null');
       assert(is.array(firstElement.properties));
+
       // Test the first property
       firstProperty = firstElement.properties[0];
       assert(is.integer(firstProperty.id));
       assert(is.string(firstProperty.name));
       assert(is.string(firstProperty.valuetype));
-      // TODO manage unit (string | null)
+      okType = false;
+      if (is.null(firstProperty.unit)) {
+        okType = true;
+      } else if (is.string(irstProperty.unit)) {
+        okType = true;
+      }
+      assert(okType, 'unit must be a string or null');
       assert(is.array(firstProperty.listvalues));
       assert(is.string(firstProperty.value));
       assert(is.boolean(firstProperty.byfusioninventory));
